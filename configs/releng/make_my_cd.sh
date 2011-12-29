9b15912045956b62bc794683e22c09d742bce3fe
Deryl R. Doucette <me@deryldoucette.com>
2011-12-29 16:03:56 -0500

    Added execution file for generating a CD image.

---
+++ configs/releng/make_my_cd.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+BUILD_TREE="$HOME/build/archlinux_isomaker/configs/releng"
+cd $BUILD_TREE
+
+# Update root-image
+cd root-image/home/arch/tcc_arch_installer
+git pull origin
+
+# Update any changes to within $BUILD_TREE
+sudo cp -R rubyists/aif_build_module $BUILD_TREE/root-image/usr/lib/aif/user/
+sudo cp rubyists/default_build/root-image/root/rubyists_configure_box $BUILD_TREE/root-image/root/
+
+cd $BUILD_TREE
+
+# Clean ArchISO work and out trees, then rebuild.
+sudo rm -f out/*.iso
+sudo bash ./build.sh purge single all
+sudo /usr/bin/time -p bash ./build.sh build single all
+
+# Now copy the core ISO off to the host for the VM(s)
+scp out/archlinux-2011.12.29-core-x86_64.iso pgpmbp:~/
+
