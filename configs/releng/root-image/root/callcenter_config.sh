79be7c530f1c2f9b7b854e60459a2bc4d4b3309e
Deryl R. Doucette <me@deryldoucette.com>
2011-12-31 19:01:40 -0500

    updated callcenter_config.sh

--- configs/releng/root-image/root/callcenter_config.sh
+++ configs/releng/root-image/root/callcenter_config.sh
@@ -3,11 +3,39 @@
 # Script executes package installs fromwithin chroot env.
 
 pacman -Syy --noconfirm
+pacman --noprogressbar --noconfirm --logfile $HOME/pacman_install_and_upgrade.log -Syu
+
+# Add 'callcenter' user now that we've populated it's homedir with the authorized_keys file.
+/usr/sbin/useradd -g users -G wheel -s /bin/bash callcenter
+# Ensure that callcenter owns it's homedir and files, then su to callcenter to start pkg builds.
+/bin/chown -R callcenter:users /home/callcenter
+
+# Configure our preferred, minimalistic, sudoers file
+# Removing here ensures sudo package has no reason to fail.
+if [ -f "/etc/sudoers" ]; then
+  rm -f /etc/sudoers
+fi
+
+pacman -S fakeroot tmux git devtools sudo --needed --noconfirm
+
+# Configure our preferred, minimalistic, sudoers file
+# Removing here ensures sudo package has no reason to fail.
+if [ -f "/etc/sudoers" ]; then
+  rm -f /etc/sudoers
+fi
+
+# Create our own sudoers file, and ensure perms and ownerships
+echo "root ALL=(ALL) ALL" > ./sudoers
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> ./sudoers
+mv -f ./sudoers /etc/sudoers
+chmod 0440 /etc/sudoers
+chown root:root /etc/sudoers
 
 # Now we start the builds themselves. Pacman should be happy now, dang it!
 echo "**** STARTING BUILDS ****"
-echo "**** STARTING BUILDS ****" > 1>&2 
+ 
 mkdir /tmp/builds
+cd /tmp/builds
 wget http://aur.archlinux.org/packages/fg/fgetty/fgetty.tar.gz
 wget http://aur.archlinux.org/packages/ru/runit-dietlibc/runit-dietlibc.tar.gz
 wget http://aur.archlinux.org/packages/ru/runit-run/runit-run.tar.gz
@@ -18,10 +46,8 @@ wget http://aur.archlinux.org/packages/fr/freeswitch-git/freeswitch-git.tar.gz
 
   # Now extract, build, create, and install AUR packages we grabbed
   # TODO: Change this to a sourced, ordered, file for package install
-/bin/su -l callcenter -c 'cd $HOME/builds ; for name in ./*.gz ; do tar -xzvf $name ; done'
-/bin/chown -R callcenter:users /home/callcenter/builds
-/bin/mknod /dev/null c 1 3
-/bin/chmod 0666 /dev/null
+for name in ./*.gz ; do tar -xzvf $name ; done
+
 cd /tmp/builds/fgetty
 makepkg -si --asroot --noconfirm
 cd /tmp/builds/runit-dietlibc
@@ -36,5 +62,3 @@ cd /tmp/builds/sv-helper
 makepkg -si --asroot --noconfirm
 cd /tmp/builds/freeswitch-git
 makepkg -si --asroot --noconfirm
-
-
